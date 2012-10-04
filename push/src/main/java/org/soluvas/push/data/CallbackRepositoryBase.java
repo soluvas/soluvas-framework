046485ad160cd1e3b1b54a2f223d05c20316a0f3
Hendy Irawan <hendy@soluvas.com>
2012-10-04 12:53:43 +0700

    Repository capabilities.

---
+++ push/src/main/java/org/soluvas/push/data/CallbackRepositoryBase.java
@@ -0,0 +1,70 @@
+package org.soluvas.push.data;
+
+import java.util.List;
+
+import org.soluvas.async.FailingCallback;
+
+/**
+ * Throws {@link UnsupportedOperationException} for all methods.
+ * @author ceefour
+ */
+public class CallbackRepositoryBase<ID, T> implements
+		CallbackRepository<ID, T> {
+
+	/* (non-Javadoc)
+	 * @see org.soluvas.push.data.CallbackRepository#add(java.lang.Object, org.soluvas.async.FailingCallback)
+	 */
+	@Override
+	public void add(T data, FailingCallback<T> callback) {
+		throw new UnsupportedOperationException();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.soluvas.push.data.CallbackRepository#delete(java.lang.Object, org.soluvas.async.FailingCallback)
+	 */
+	@Override
+	public void delete(ID id, FailingCallback<T> callback) {
+		throw new UnsupportedOperationException();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.soluvas.push.data.CallbackRepository#findOne(java.lang.Object, org.soluvas.async.FailingCallback)
+	 */
+	@Override
+	public void findOne(ID id, FailingCallback<T> callback) {
+		throw new UnsupportedOperationException();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.soluvas.push.data.CallbackRepository#findAll(org.soluvas.async.FailingCallback)
+	 */
+	@Override
+	public void findAll(FailingCallback<List<T>> callback) {
+		throw new UnsupportedOperationException();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.soluvas.push.data.CallbackRepository#findAllIds(org.soluvas.async.FailingCallback)
+	 */
+	@Override
+	public void findAllIds(FailingCallback<List<ID>> callback) {
+		throw new UnsupportedOperationException();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.soluvas.push.data.CallbackRepository#search(java.lang.String, org.soluvas.async.FailingCallback)
+	 */
+	@Override
+	public void search(String searchText, FailingCallback<List<T>> callback) {
+		throw new UnsupportedOperationException();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.soluvas.push.data.CallbackRepository#update(java.lang.Object, java.lang.Object, org.soluvas.async.FailingCallback)
+	 */
+	@Override
+	public void update(ID id, T data, FailingCallback<T> callback) {
+		throw new UnsupportedOperationException();
+	}
+
+}
