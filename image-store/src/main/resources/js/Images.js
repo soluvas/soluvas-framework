43c9519fee784ab27c325e06be5715bb414c9bac
Rudi Wijaya <rudi_mail90@yahoo.com>
2012-10-22 12:53:15 +0700

    Refs #723, #730, #731. Summary: 1. Customer UI SYS - Add

--- image-store/src/main/resources/js/Images.js
+++ image-store/src/main/resources/js/Images.js
@@ -2,15 +2,21 @@ define(['org.soluvas.image/imageConfigs', 'uritemplate'], function(imageConfigs,
 
 var Images = {
 	getPublicUri: function(namespace, imageId, styleCode, variantCode, ext) {
-		if (typeof(namespace) === 'undefined') {
+		if (typeof namespace === 'undefined') {
 			console.error('org.soluvas.web/Image: Namespace must be provided');
+			return null;
 		}
-		if (typeof(imageId) === 'undefined') {
+		if (typeof imageId === 'undefined') {
 			console.error('org.soluvas.web/Image: Namespace must be provided');
+			return null;
 		}
 		var styleCode = typeof styleCode !== 'undefined' ? styleCode : 't';
 		var styleVariant = typeof variantCode !== 'undefined' ? styleCode + '_' + variantCode : styleCode;
 		var ext = typeof ext !== 'undefined' ? ext : 'jpg';
+		if (typeof imageConfigs[namespace] === 'undefined') {
+			console.error('imageConfigs does not contain namespace', namespace);
+			return null;
+		}
 		return imageConfigs[namespace].publicUriExpander.expand({namespace: namespace, imageId: imageId,
 			styleCode: styleCode, variantCode: variantCode, styleVariant: styleVariant, ext: ext});
 	},
--- image-store/src/main/resources/js/imageConfigs.js
+++ image-store/src/main/resources/js/imageConfigs.js
@@ -3,8 +3,16 @@ define(['uritemplate'], function(UriTemplate) {
 
 var imageConfigs = {
 	'person': {
-		publicUriTemplate: 'http://dav.berbatik.annafi.dev/{namespace}/{styleCode}/{imageId}_{styleVariant}.{ext}',
-		publicUriExpander: UriTemplate.parse('http://dav.berbatik.annafi.dev/{namespace}/{styleCode}/{imageId}_{styleVariant}.{ext}'),
+		publicUriTemplate: 'http://dav.berbatik.rudi.dev/{namespace}/{styleCode}/{imageId}_{styleVariant}.{ext}',
+		publicUriExpander: UriTemplate.parse('http://dav.berbatik.rudi.dev/{namespace}/{styleCode}/{imageId}_{styleVariant}.{ext}'),
+	},
+	'product': {
+		publicUriTemplate: 'http://dav.berbatik.rudi.dev/{namespace}/{styleCode}/{imageId}_{styleVariant}.{ext}',
+		publicUriExpander: UriTemplate.parse('http://dav.berbatik.rudi.dev/{namespace}/{styleCode}/{imageId}_{styleVariant}.{ext}'),
+	},
+	'shop': {
+		publicUriTemplate: 'http://dav.berbatik.rudi.dev/{namespace}/{styleCode}/{imageId}_{styleVariant}.{ext}',
+		publicUriExpander: UriTemplate.parse('http://dav.berbatik.rudi.dev/{namespace}/{styleCode}/{imageId}_{styleVariant}.{ext}'),
 	},
 };
 
