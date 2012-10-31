332752d60f5dd23494f1c1fa805283cfe52ba62e
Hendy Irawan <hendy@soluvas.com>
2012-10-31 13:44:22 +0700

    Generic EMF unloader.

---
+++ commons/src/main/java/org/soluvas/commons/EmfUnloader.java
@@ -0,0 +1,56 @@
+package org.soluvas.commons;
+
+import org.eclipse.emf.ecore.EPackage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Generic EMF Unloader.
+ * 
+ * Usage:
+ * 
+ * <pre>
+ * {@code
+ * <bean class="org.soluvas.commons.EmfUnloader" destroy-method="destroy">
+ * 	<argument value="org.soluvas.security.SecurityPackage" />
+ * </bean>
+ * }
+ * </pre>
+ * 
+ * @author ceefour
+ */
+public class EmfUnloader {
+
+	private transient Logger log = LoggerFactory.getLogger(EmfUnloader.class);
+	private final Class<? extends EPackage>[] packages;
+
+	@SuppressWarnings("unchecked")
+	public EmfUnloader(Class<? extends EPackage> pkg) {
+		super();
+		this.packages = new Class[] { pkg };
+	}
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public EmfUnloader(Class... packages) {
+		super();
+		this.packages = packages;
+	}
+
+	/**
+	 * Call this to unload the packages.
+	 */
+	public void destroy() {
+		for (Class<? extends EPackage> pkg : packages) {
+			try {
+				String packageNsUri = (String) pkg.getDeclaredField("eNS_URI")
+						.get(pkg);
+				log.debug("Unloading EMF Package {} by {}", packageNsUri,
+						pkg.getName());
+				EPackage.Registry.INSTANCE.remove(packageNsUri);
+			} catch (Exception e) {
+				log.error("Cannot unload EMF Package " + pkg.getName(), e);
+			}
+		}
+	}
+
+}
