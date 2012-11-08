bdc1a7710ac14b31231dde68cde2f519b60627d8
Rudi Wijaya <rudi_mail90@yahoo.com>
2012-11-08 17:48:42 +0700

    Refs #723, #730, #731. Summary: 1. Product Cart: show only, not finish for other actions.

--- commons/src/main/java/org/soluvas/commons/EventBusSubscriberTracker.java
+++ commons/src/main/java/org/soluvas/commons/EventBusSubscriberTracker.java
@@ -10,6 +10,63 @@ import com.google.common.eventbus.EventBus;
 
 /**
  * Tracks {@link EventBus} subscribers.
+ * 
+ * EventBus service:
+ * 
+ * <pre>{@code
+ * <bean id="eventBus" class="com.google.common.eventbus.EventBus">
+ * 		<argument value="berbatik_dev"/>
+ * 	</bean>
+ * 	<service ref="eventBus" auto-export="all-classes">
+ * 		<service-properties>
+ * 			<entry key="clientId" value="berbatik"/>
+ * 			<entry key="tenantEnv" value="dev"/>
+ * 			<entry key="tenantId" value="berbatik"/>
+ * 			<entry key="namespace" value=""/>
+ * 		</service-properties>
+ * 	</service>
+ * 	<bean class="org.osgi.util.tracker.ServiceTracker" init-method="open" destroy-method="close">
+ * 		<argument ref="blueprintBundleContext"/>
+ * 		<argument>
+ * 			<bean class="org.osgi.framework.FrameworkUtil" factory-method="createFilter">
+ * 				<argument value="(&amp;(objectClass=com.google.common.base.Supplier)(tenantId=berbatik)(tenantEnv=dev)(type=eventbus))"/>
+ * 			</bean>
+ * 		</argument> 
+ * 		<argument>
+ * 			<bean class="org.soluvas.eventbus.EventBusSubscriberTracker">
+ * 				<argument ref="eventBus" />
+ * 			</bean>
+ * 		</argument>
+ * 	</bean>
+ * }</pre>
+ * 
+ * Registering subscriber:
+ * 
+ * <pre>{@code
+ * <service auto-export="interfaces">
+ * 		<service-properties>
+ * 			<entry key="type" value="eventbus"/>
+ * 			<entry key="clientId" value="berbatik"/>
+ * 			<entry key="tenantId" value="berbatik"/>
+ * 			<entry key="tenantEnv" value="dev"/>
+ * 		</service-properties>
+ * 		<bean class="com.google.common.base.Suppliers" factory-method="ofInstance">
+ * 			<argument>
+ * 				<bean class="tmp.test.subscriber.TestSubscriber" />
+ * 			</argument>
+ * 		</bean>
+ * 	</service>
+ * }</pre>
+ * 
+ * Consuming EventBus:
+ * 
+ * <pre>{@code
+ * <reference id="eventBus" interface="com.google.common.eventbus.EventBus"
+ * 		filter="(&amp;(tenantId=berbatik)(tenantEnv=dev))"/>
+ * }</pre>
+ * 
+ * See also {@literal README.textile} for more info.
+ * 
  * @author rudi
  */
 public class EventBusSubscriberTracker implements ServiceTrackerCustomizer {
