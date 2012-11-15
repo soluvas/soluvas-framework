5401a87d88d858a0609bdc564c3e49bbd7209c84
Hendy Irawan <hendy@soluvas.com>
2012-11-15 14:55:16 +0700

    OPEN - task 5: Decouple Wicket component hierarchy using placeholders http://github.com/soluvas/soluvas-web/issues/issue/5

---
+++ commons/src/main/java/org/soluvas/commons/SerializableEObject.java
@@ -0,0 +1,13 @@
+package org.soluvas.commons;
+
+import java.io.Serializable;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * Simply as it name says, please also make contructors public in your GenModel.
+ * @author ceefour
+ */
+public interface SerializableEObject extends EObject, Serializable {
+
+}
