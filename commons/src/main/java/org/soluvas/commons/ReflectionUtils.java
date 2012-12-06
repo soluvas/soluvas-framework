f567c4ea6c8039809e3d09ecbef78a2aa6da2de2
Rully Lukman <rully@bippo.co.id>
2012-12-06 10:46:41 +0700

    NEW - # 4: JAX-RS @ApiResource @Inject @Supplied support https://bitbucket.org/bippo/bippo-commerce5/issue/4/

--- commons/src/main/java/org/soluvas/commons/ReflectionUtils.java
+++ commons/src/main/java/org/soluvas/commons/ReflectionUtils.java
@@ -1,11 +1,20 @@
 package org.soluvas.commons;
 
 import java.lang.reflect.Field;
+import java.util.Collection;
 import java.util.List;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.soluvas.commons.inject.Supplied;
 
 import com.google.common.base.Preconditions;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Collections2;
 import com.google.common.collect.ImmutableList;
 
 /**
@@ -15,9 +24,12 @@ import com.google.common.collect.ImmutableList;
  */
 public class ReflectionUtils {
 
+	private static final Logger log = LoggerFactory
+			.getLogger(ReflectionUtils.class);
+	
 	/**
 	 * Return all {@link Field}s (including private) of a {@link Class},
-	 * including superclasses. Useful for object mapping.
+	 * including superclasses. Useful for object mapping and dependency injection.
 	 * @param clazz
 	 * @return
 	 */
@@ -40,6 +52,72 @@ public class ReflectionUtils {
 		fieldsBuilder.addAll(ImmutableList.copyOf(ownFields));
 		
 		return fieldsBuilder.build();
+	}
+
+	/**
+	 * Return all @{@link Inject} {@link Field}s (including private) of a {@link Class},
+	 * including superclasses. Useful for dependency injection.
+	 * @param clazz
+	 * @return
+	 */
+	public static List<Field> getInjectFields(@Nonnull Class<?> clazz) {
+		final List<Field> allFields = getAllFields(clazz);
+		final Collection<Field> fields = Collections2.filter(allFields,
+				new Predicate<Field>() {
+					@Override
+					public boolean apply(@Nullable Field field) {
+						final Inject injectAnn = field.getAnnotation(Inject.class);
+						return injectAnn != null;
+					}
+				});
+		final List<Field> fieldList = ImmutableList.copyOf(fields);
+		log.trace("{} has {} @Inject fields: {}", clazz,
+				fieldList.size(), fieldList);
+		return fieldList;
+	}
+	
+	/**
+	 * Return all non-@{@link Supplied} @{@link Inject} {@link Field}s (including private) of a {@link Class},
+	 * including superclasses. Useful for dependency injection.
+	 * @param clazz
+	 * @return
+	 */
+	public static List<Field> getNonSuppliedFields(@Nonnull Class<?> clazz) {
+		final List<Field> injectFields = getInjectFields(clazz);
+		final Collection<Field> fields = Collections2.filter(injectFields,
+				new Predicate<Field>() {
+					@Override
+					public boolean apply(@Nullable Field field) {
+						final Supplied suppliedAnn = field.getAnnotation(Supplied.class);
+						return suppliedAnn == null;
+					}
+				});
+		final List<Field> fieldList = ImmutableList.copyOf(fields);
+		log.trace("{} has {} non-@Supplied fields: {}", clazz,
+				fieldList.size(), fieldList);
+		return fieldList;
+	}
+
+	/**
+	 * Return all @{@link Supplied} {@link Inject} {@link Field}s (including private) of a {@link Class},
+	 * including superclasses. Useful for dependency injection.
+	 * @param clazz
+	 * @return
+	 */
+	public static List<Field> getSuppliedFields(@Nonnull Class<?> clazz) {
+		final List<Field> injectFields = getInjectFields(clazz);
+		final Collection<Field> fields = Collections2.filter(injectFields,
+				new Predicate<Field>() {
+					@Override
+					public boolean apply(@Nullable Field field) {
+						final Supplied suppliedAnn = field.getAnnotation(Supplied.class);
+						return suppliedAnn != null;
+					}
+				});
+		final List<Field> fieldList = ImmutableList.copyOf(fields);
+		log.trace("{} has {} @Supplied fields: {}", clazz,
+				fieldList.size(), fieldList);
+		return fieldList;
 	}
 	
 }
---
+++ commons/src/main/java/org/soluvas/commons/tenant/TenantInjection.java
@@ -0,0 +1,141 @@
+package org.soluvas.commons.tenant;
+
+import java.lang.reflect.Field;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.annotation.Nonnull;
+import javax.inject.Inject;
+
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.soluvas.commons.ReflectionUtils;
+import org.soluvas.commons.inject.Filter;
+import org.soluvas.commons.inject.Namespace;
+
+import com.google.common.base.Throwables;
+
+/**
+ * Base implementation to injects {@link Inject} {@link TenantRef} services using lifecycle methods
+ * inject and uninject.
+ * @author ceefour
+ */
+public class TenantInjection {
+	
+	private static Logger log = LoggerFactory
+			.getLogger(TenantInjection.class);
+	private final BundleContext bundleContext;
+	private final String tenantId;
+	private final String tenantEnv;
+	/**
+	 * List of get-ed services (to unget).
+	 */
+	private final Map<Field, ServiceReference<?>> serviceRefs = new HashMap<Field, ServiceReference<?>>();
+	
+	/**
+	 * @param bundleContext
+	 * @param tenantId
+	 * @param tenantEnv
+	 */
+	public TenantInjection(@Nonnull final BundleContext bundleContext,
+			@Nonnull final String tenantId, @Nonnull final String tenantEnv) {
+		super();
+		this.bundleContext = bundleContext;
+		this.tenantId = tenantId;
+		this.tenantEnv = tenantEnv;
+	}
+	
+	/**
+	 * Perform injection.
+	 * @param component
+	 * @param phase 
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public void inject(@Nonnull final Object component, @Nonnull final String componentId, @Nonnull final String phase) {
+		final List<Field> nonSuppliedFields = ReflectionUtils.getNonSuppliedFields(component.getClass());
+		for (final Field field : nonSuppliedFields) {
+			try {
+				Object currentValue = FieldUtils.readField(field, component, true);
+				if (currentValue != null)
+					continue;
+			} catch (IllegalAccessException e) {
+				Throwables.propagate(e);
+				return;
+			}
+			
+			final Namespace namespaced = field.getAnnotation(Namespace.class);
+			final String namespace = namespaced != null ? namespaced.value() : null;
+			final String namespaceFilter = namespace != null ? "(namespace=" + namespace + ")" : "";
+			
+			final Filter filtered = field.getAnnotation(Filter.class);
+			final String additionalFilter = filtered != null ? filtered.value() : "";
+			
+			final Class serviceClass = field.getType();
+			
+			log.trace("Field {}#{} looking up {} for tenantId={} tenantEnv={} namespace={} filter: {}",
+					componentId, field.getName(), serviceClass.getName(), tenantId, tenantEnv, namespace, additionalFilter );
+			final String filter = String.format("(&(|(tenantId=%s)(tenantId=\\*))(|(tenantEnv=%s)(tenantEnv=\\*)))",
+					tenantId, tenantEnv, namespaceFilter, additionalFilter);
+			
+			final ServiceReference<?> serviceRef;
+			try {
+				final Collection<ServiceReference<?>> foundRefs = bundleContext.getServiceReferences(serviceClass, filter);
+				if (foundRefs == null || foundRefs.isEmpty()) {
+					throw new RuntimeException("Cannot inject " + componentId + "#" + field.getName() + ", " +
+							serviceClass.getName() + " service with " + filter + " not found");
+				}
+				serviceRef = foundRefs.iterator().next();
+			} catch (InvalidSyntaxException e) {
+				throw new RuntimeException("Cannot inject " + componentId + "#" + field.getName() + ", invalid " +
+						serviceClass.getName() + " service with " + filter);
+			}
+			final Object bean = bundleContext.getService(serviceRef);
+			try {
+				log.trace("Injecting {}#{} as {}", componentId, field.getName(), bean);
+				FieldUtils.writeField(field, component, bean, true);
+				serviceRefs.put(field, serviceRef);
+			} catch (Exception e) {
+				bundleContext.ungetService(serviceRef);
+				serviceRefs.remove(field);
+				throw new RuntimeException("Cannot set field " + componentId + "#" + field.getName() + " using " +
+						serviceClass.getName() + " service with " + filter, e);
+			}
+		}
+		if (!serviceRefs.isEmpty()) {
+			log.debug("Injected {} services to {} in {}", serviceRefs.size(), componentId, phase);
+		}
+	}
+	
+	/**
+	 * Uninjects injected fields.
+	 * @param component
+	 * @param phase 
+	 */
+	public void uninject(@Nonnull final Object component, @Nonnull final String componentId, @Nonnull final String phase) {
+		if (!serviceRefs.isEmpty()) {
+			log.debug("Uninjecting {} services from {} due to {}", serviceRefs.size(), componentId, phase);
+			final Iterator<Entry<Field, ServiceReference<?>>> serviceRefIterator = serviceRefs.entrySet().iterator();
+			while (serviceRefIterator.hasNext()) {
+				final Entry<Field, ServiceReference<?>> entry = serviceRefIterator.next();
+				final Field field = entry.getKey();
+				log.trace("Unsetting {}#{}", componentId, field.getName() );
+				try {
+					FieldUtils.writeField(field, component, null, true);
+				} catch (Exception e) {
+					log.warn("Cannot unset " + componentId + "#" + field.getName(), e);
+				}
+				bundleContext.ungetService(entry.getValue());
+				serviceRefIterator.remove();
+			}
+		}
+	}
+
+}
