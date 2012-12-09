26b6db4c311ec36545dec84f69402e83290bea84
Rully Lukman <rully@bippo.co.id>
2012-12-09 07:31:49 +0700

    IdFunction. NEW - # 229: Separate Story into com.soluvas.story https://bitbucket.org/bippo/bippo-commerce5/issue/229/

---
+++ commons/src/main/java/org/soluvas/commons/IdFunction.java
@@ -0,0 +1,21 @@
+package org.soluvas.commons;
+
+import javax.annotation.Nullable;
+
+import com.google.common.base.Function;
+
+/**
+ * Return {@link Identifiable#getId()}.
+ * @author rully
+ */
+public class IdFunction implements Function<Identifiable, String> {
+
+	/* (non-Javadoc)
+	 * @see com.google.common.base.Function#apply(java.lang.Object)
+	 */
+	@Override @Nullable
+	public String apply(@Nullable Identifiable input) {
+		return input != null ? input.getId() : null;
+	}
+
+}
