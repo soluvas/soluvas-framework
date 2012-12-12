ead214be7e43ce337036e34440ab48212475dc01
Rully Lukman <rully@bippo.co.id>
2012-12-13 04:50:24 +0700

    mustache & stringtemplate dependencies.
    OPEN - task refs #27: Declarative email system 
    http://github.com/soluvas/soluvas-framework/issues/issue/27

---
+++ email.shell/src/main/java/org/soluvas/email/shell/EmailTemplateLs.java
@@ -0,0 +1,16 @@
+package org.soluvas.email.shell;
+
+/**
+ * @author rully
+ *
+ */
+public class EmailTemplateLs {
+
+	/**
+	 * 
+	 */
+	public EmailTemplateLs() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
