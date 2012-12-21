2c270a308b75b36414556046ce4a7338e3f653c1
Rudi Wijaya <rudi_mail90@yahoo.com>
2012-12-21 17:34:51 +0700

    Summary: 1. Refactoring UI Product Sys is not complete.

---
+++ json/src/main/java/org/soluvas/json/EMapDeserializer.java
@@ -0,0 +1,58 @@
+package org.soluvas.json;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.emf.common.util.BasicEList;
+import org.eclipse.emf.common.util.BasicEMap;
+import org.eclipse.emf.common.util.EMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.google.common.base.Function;
+import com.google.common.collect.Maps;
+
+/**
+ * Deserializer class that can deserialize instances of
+ * specified Enum class from Strings and Integers,
+ * uppercasing before deserialization.
+ * @author ceefour
+ */
+@SuppressWarnings("serial")
+public class EMapDeserializer extends StdDeserializer<EMap<Object, Object>> {
+
+	private static final Logger log = LoggerFactory
+			.getLogger(EMapDeserializer.class);
+//	private final MapDeserializer mapDeserializer;
+	
+	public EMapDeserializer() {
+		super(EMap.class);
+//		mapDeserializer = new MapDeserializer(null, null, null, null, null);
+	}
+	
+	@Override
+	public EMap<Object, Object> deserialize(JsonParser jp, DeserializationContext ctxt)
+			throws IOException, JsonProcessingException {
+		JsonDeserializer<Map> mapDeser = (JsonDeserializer) ctxt.findRootValueDeserializer(ctxt.getTypeFactory().constructType(Map.class));
+		final Map<Object, Object> map = mapDeser.deserialize(jp, ctxt);
+		// convert List to EList
+		final Map<Object, Object> transformed = Maps.transformValues(map, new Function<Object, Object>() {
+			@Override
+			public Object apply(Object input) {
+				if (input == null)
+					return null;
+				if (input instanceof List)
+					return new BasicEList((List) input);
+				return null;
+			}
+		});
+		return new BasicEMap<Object, Object>(transformed);
+	}
+
+}
