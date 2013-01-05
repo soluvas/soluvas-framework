5f5d968b90c4148a31cb969e504585cc1c04e1d6
Ahmad Syarif Farsiado <farsiado@berbatik.com>
2013-01-05 17:11:08 +0700

    OPEN - task refs #26: Shiro security Subject integration with JAX-RS resources  http://github.com/soluvas/soluvas-framework/issues/issue/26

--- security/src/main/java/org/soluvas/security/SoluvasSecurityManager.java
+++ security/src/main/java/org/soluvas/security/SoluvasSecurityManager.java
@@ -3,6 +3,7 @@ package org.soluvas.security;
 import java.util.Collection;
 
 import org.apache.shiro.realm.Realm;
+import org.apache.shiro.session.mgt.SessionKey;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.subject.SubjectContext;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
@@ -32,11 +33,30 @@ public class SoluvasSecurityManager extends DefaultWebSecurityManager {
 
 	@Override
 	public Subject createSubject(SubjectContext subjectContext) {
-		log.trace("BEFORE subjectContext.sessionId = {}", subjectContext.getSessionId());
+		log.trace("BEFORE createSubject: subjectContext.session={}, subjectContext.sessionId = {}", 
+				subjectContext.getSession(), subjectContext.getSessionId());
 		final Subject subject = super.createSubject(subjectContext);
-		log.trace("AFTER subjectContext.sessionId = {}; subject.session.id = {}", subjectContext.getSessionId(),
+		log.trace("AFTER createSubject: subjectContext.session={}, subjectContext.sessionId={}, subject.session.id = {}",
+				subjectContext.getSession(), subjectContext.getSessionId(),
 				subject.getSession().getId());
 		return subject;
+	}
+	
+	@Override
+	protected SubjectContext resolveSession(SubjectContext context) {
+		log.trace("BEFORE resolveSession: context.session = {}, sessionId={}",
+				context.getSession(), context.getSessionId());
+		SubjectContext newContext = super.resolveSession(context);
+		log.trace("AFTER resolveSession: context.session = {}, sessionId={}",
+				newContext.getSession(), newContext.getSessionId());
+		return newContext;
+	}
+	
+	@Override
+	protected SessionKey getSessionKey(SubjectContext context) {
+		SessionKey sessionKey = super.getSessionKey(context);
+		log.trace("SessionKey={}", sessionKey);
+		return sessionKey;
 	}
 
 }
