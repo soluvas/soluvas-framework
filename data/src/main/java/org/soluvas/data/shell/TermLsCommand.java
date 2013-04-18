f16bd4545d73c4331880b6cbc165f1045246c368
Rudi Wijaya <rudi_mail90@yahoo.com>
2013-04-18 14:59:45 +0700

    yesCallBack.onYes(target);

--- data/src/main/java/org/soluvas/data/shell/TermLsCommand.java
+++ data/src/main/java/org/soluvas/data/shell/TermLsCommand.java
@@ -2,27 +2,37 @@ package org.soluvas.data.shell;
 
 import static org.fusesource.jansi.Ansi.ansi;
 
+import javax.inject.Inject;
+
 import org.apache.felix.gogo.commands.Command;
-import org.apache.karaf.shell.console.OsgiCommandSupport;
-import org.osgi.framework.ServiceReference;
 import org.soluvas.commons.NameUtils;
+import org.soluvas.commons.shell.ExtCommandSupport;
 import org.soluvas.data.DataCatalog;
 import org.soluvas.data.Term;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
 
 /**
  * Display all {@link Term}s.
  * @author rully
  */
+@Service @Lazy
 @Command(scope="term", name="ls", description="Display all Terms.")
-public class TermLsCommand extends OsgiCommandSupport {
+public class TermLsCommand extends ExtCommandSupport {
+	
+	private final DataCatalog dataCatalog;
+
+	@Inject
+	public TermLsCommand(DataCatalog dataCatalog) {
+		super();
+		this.dataCatalog = dataCatalog;
+	}
 
 	/* (non-Javadoc)
 	 * @see org.apache.karaf.shell.console.AbstractAction#doExecute()
 	 */
 	@Override
 	protected Object doExecute() throws Exception {
-		final ServiceReference<DataCatalog> dataCatalogRef = bundleContext.getServiceReference(DataCatalog.class);
-		final DataCatalog dataCatalog = getService(DataCatalog.class, dataCatalogRef);
 		System.out.println(ansi().render("@|negative_on %3s|%-10s|%-15s|%-20s|%-10s|%-10s|%-20s|@",
 				"№", "NsPrefix", "Name", "Display Name", "Kind NsP", "Kind Name", "Bundle"));
 		int i = 0;
