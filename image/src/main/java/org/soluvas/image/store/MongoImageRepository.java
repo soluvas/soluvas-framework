4545f42af4b08767c4bca17113e5b5fe6433cbad
Rudi Wijaya <rudi_mail90@yahoo.com>
2013-08-26 21:08:36 +0700

    IN PROGRESS - issue BC-1236: Halaman release malah menampilkan produk onSale, seharusnya menampilkan daftar produk untuk release tersebut https://idbippo.atlassian.net/browse/BC-1236

--- image/src/main/java/org/soluvas/image/store/MongoImageRepository.java
+++ image/src/main/java/org/soluvas/image/store/MongoImageRepository.java
@@ -815,7 +815,7 @@ public class MongoImageRepository extends PagingAndSortingRepositoryBase<Image, 
 					return input.getId();
 				}
 			});
-			log.debug("Got {} {} images with IDs {}", 
+			log.trace("Got {} {} images with IDs {}", 
 					images.size(), namespace, ids );
 			return images;
 		} finally {
