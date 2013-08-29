49acbc2e0e1a2715e024110b88fc998fb1a942d6
Hendy Irawan <ceefour666@gmail.com>
2013-08-29 13:36:00 +0700

    mailjet fix

--- newsletter/src/main/java/org/soluvas/newsletter/Mailjet.java
+++ newsletter/src/main/java/org/soluvas/newsletter/Mailjet.java
@@ -36,7 +36,10 @@ public class Mailjet implements Serializable {
 	private UsernamePasswordCredentials userCredentials;
 	private DefaultHttpClient httpClient;
 	
-	final ObjectMapper mapper = JsonUtils.mapper;
+	private final ObjectMapper mapper = JsonUtils.mapper;
+	
+	private String errorMessage;
+	private AddContactResponse addContactRespone;
 	
 	public Mailjet(MailjetManager mailjetManager) {
 		userCredentials = new UsernamePasswordCredentials(mailjetManager.getApiKey(),
@@ -59,6 +62,7 @@ public class Mailjet implements Serializable {
 	 * @param email
 	 * @return
 	 */
+	@Deprecated
 	public AddContactResponse addContact(long listId, String email) {
 		ArrayList<NameValuePair> formParams = new ArrayList<NameValuePair>();
 		formParams.add(new BasicNameValuePair("contact", email));
@@ -72,12 +76,10 @@ public class Mailjet implements Serializable {
 			log.info("sending request to add contact");
 			HttpResponse httpResponse = httpClient.execute(httpPost);
 			log.debug("HttpResponse status code {}", httpResponse.getStatusLine().getStatusCode());
-			
-			if (httpResponse.getStatusLine().getStatusCode() != 200 )
+			if (httpResponse.getStatusLine().getStatusCode() != 200 ){
 				throw new IOException(String.format("throw Http Error %s", 
-						httpResponse.getStatusLine().getReasonPhrase())
-						);
-			
+						httpResponse.getStatusLine().getReasonPhrase()));
+			}
 			final String responseContent = IOUtils.toString(httpResponse.getEntity().getContent());
 			return mapper.readValue(responseContent, AddContactResponse.class);
 			
@@ -85,6 +87,43 @@ public class Mailjet implements Serializable {
 			throw new RuntimeException("Cannot add contact "+ email, ex);
 		}
 	}
+	
+	/**
+	 * 
+	 * @param listId
+	 * @param email
+	 * @return
+	 */
+	public boolean insertContact(long listId, String email) {
+		ArrayList<NameValuePair> formParams = new ArrayList<NameValuePair>();
+		formParams.add(new BasicNameValuePair("contact", email));
+		formParams.add(new BasicNameValuePair("id", String.valueOf(listId)));
+		
+		try {
+			UrlEncodedFormEntity entity = new UrlEncodedFormEntity(formParams, "UTF-8");
+			HttpPost httpPost = new HttpPost(apiUri + "listsAddcontact/?output=json");
+			httpPost.setEntity(entity);
+
+			HttpResponse httpResponse = httpClient.execute(httpPost);
+			log.info("HttpResponse status code {}", httpResponse.getStatusLine().getStatusCode());
+			
+			if (httpResponse.getStatusLine().getStatusCode() != 200 ){
+				final int statusCode = httpResponse.getStatusLine().getStatusCode();
+				final String messageDesc = getMessageDescription(statusCode);
+				setErrorMessage(messageDesc);
+				return false;
+			} else {
+				final String responseContent = IOUtils.toString(httpResponse.getEntity().getContent());
+				final AddContactResponse addedContact = mapper.readValue(responseContent, AddContactResponse.class);
+				setAddContactRespone(addedContact);
+				return true;
+			}
+			
+		} catch (Exception ex) {
+			throw new RuntimeException("Cannot add contact "+ email, ex);
+		}
+	}
+
 	
 	/**
 	 * add contact to certain contact's id
@@ -147,5 +186,67 @@ public class Mailjet implements Serializable {
 			throw new RuntimeException("Error when create new list contact ", ex);
 		}
 	}
+	
+	/**
+	 * 
+	 * @param statusCode
+	 * @return
+	 */
+	private String getMessageDescription(int statusCode) {
+		switch (statusCode) {
+		case 500 :
+			return "Unknown error occurs";
+		case 200 : 
+			return "All gone well. Congrats";
+		case 201 : 
+			return "A new insertion was done";
+		case 204 :
+			return "No content found or expected to return";
+		case 304 :
+			return "Nothing was modified";
+		case 400 :
+			return "One or more arguments are missing or maybe mispelling";
+		case 401 :
+			return "You have specified an incorrect Apikey or username/password couple";
+		case 404 :
+			return "The method you are trying to reach don't exists ";
+		case 405 :
+			return "Method not allowed";
+		default :
+				return "Unknown status code";
+			
+		}
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+	
+	/**
+	 * 
+	 * @param errorMessage
+	 */
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+
+	/**
+	 * @return the addContactRespone
+	 */
+	public AddContactResponse getAddContactRespone() {
+		return addContactRespone;
+	}
+
+	/**
+	 * @param addContactRespone the addContactRespone to set
+	 */
+	public void setAddContactRespone(AddContactResponse addContactRespone) {
+		this.addContactRespone = addContactRespone;
+	}
+
 	
 }
