66b1203026bd110d0998f382a1b917f49758b578
Hendy Irawan <ceefour666@gmail.com>
2013-09-05 18:55:13 +0700

    GRD-16 #comment PersonLsCommand

--- data/src/main/java/org/soluvas/data/person/shell/PersonLsCommand.java
+++ data/src/main/java/org/soluvas/data/person/shell/PersonLsCommand.java
@@ -5,6 +5,7 @@ import static org.fusesource.jansi.Ansi.ansi;
 import javax.inject.Inject;
 
 import org.apache.felix.gogo.commands.Command;
+import org.apache.felix.gogo.commands.Option;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.soluvas.commons.Email;
@@ -13,23 +14,31 @@ import org.soluvas.commons.Person;
 import org.soluvas.commons.shell.ExtCommandSupport;
 import org.soluvas.data.domain.Page;
 import org.soluvas.data.domain.PageRequest;
+import org.soluvas.data.domain.Sort.Direction;
 import org.soluvas.data.person.PersonRepository;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 /**
- * Shell command to list entities of {@link BippoPerson}.
- *
- * Person entity for Bippo Commerce.
+ * Shell command to list entities of {@link Person}.
  *
  * @author ceefour
  */
 @Service @Scope("prototype")
-@Command(scope="person", name="ls", description="List BippoPerson entities.")
+@Command(scope="person", name="ls", description="List Person entities.")
 public class PersonLsCommand extends ExtCommandSupport {
 
 	private static final Logger log = LoggerFactory.getLogger(PersonLsCommand.class);
 
+	@Option(name="--page", description="Page number.")
+	private transient long pageNumber = 0;
+	@Option(name="--pagesize", description="Page size.")
+	private transient long pageSize = 100;
+	@Option(name="--sort", description="Sort property.")
+	private transient String sortProperty = "modificationTime";
+	@Option(name="--sortdir", description="Sort direction.")
+	private transient Direction sortDir = Direction.DESC;
+	
 	private final PersonRepository personRepo;
 	
 	@Inject
@@ -41,8 +50,8 @@ public class PersonLsCommand extends ExtCommandSupport {
 	@Override
 	protected Object doExecute() throws Exception {
 		System.out.println(ansi().render("@|negative_on %3s|%-15s|%-15s|%-21s|%-20s|@",
-				"#", "ID", "Slug", "Name", "Email(s)" ));
-		final Page<Person> personPage = personRepo.findAll(new PageRequest(0, 1000l));
+				"№", "ID", "Slug", "Name", "Email(s)" ));
+		final Page<Person> personPage = personRepo.findAll(new PageRequest(pageNumber, pageSize, sortDir, sortProperty));
 		int i = 0;
 		for (Person it : personPage.getContent()) {
 			final String genderStr = it.getGender() == Gender.MALE ? "@|bold,blue ♂|@" : it.getGender() == Gender.FEMALE ? "@|bold,magenta ♀|@" : " ";
@@ -61,8 +70,8 @@ public class PersonLsCommand extends ExtCommandSupport {
 			System.out.println(ansi().render("@|bold,black %3d||@@|bold %-15s|@@|bold,black ||@%-15s@|bold,black ||@" + genderStr + "%-20s@|bold,black ||@%-20s",
 				++i, it.getId(), it.getSlug(), it.getName(), emails) );
 		}
-		System.out.println(ansi().render("@|bold %d|@ Person entities",
-			personPage.getTotalElements()));
+		System.out.println(ansi().render("@|bold %d|@ of @|bold %d|@ Person entities",
+			personPage.getNumberOfElements(), personPage.getTotalElements()));
 		return personPage.getTotalElements();
 	}
 
