253e49f5acb83f574b3195065c74a8e79f26862b
Rudi Wijaya <rudi_mail90@yahoo.com>
2013-09-17 21:16:46 +0700

    BC-1354 #comment Fix some miss.

--- data/src/main/java/org/soluvas/data/domain/PageRequest.java
+++ data/src/main/java/org/soluvas/data/domain/PageRequest.java
@@ -159,4 +159,10 @@ public class PageRequest implements Pageable, Serializable {
 
 		return result;
 	}
+
+	@Override
+	public String toString() {
+		return "PageRequest [page=" + page + ", size=" + size + ", "
+				+ (sort != null ? "sort=" + sort : "") + "]";
+	}
 }
