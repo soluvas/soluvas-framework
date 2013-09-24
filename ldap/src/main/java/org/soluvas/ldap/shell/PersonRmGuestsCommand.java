e337a5ded60350ed115c6b09b1ea2ec23fe8b97b
Hendy Irawan <ceefour666@gmail.com>
2013-09-24 22:47:45 +0700

    BC-1336 comment ketinggalan

--- ldap/src/main/java/org/soluvas/ldap/shell/PersonRmGuestsCommand.java
+++ ldap/src/main/java/org/soluvas/ldap/shell/PersonRmGuestsCommand.java
@@ -16,7 +16,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 /**
- * Fix invalid primaryShippingAddress and primaryBillingAddress on {@link SocialPerson} entries.
+ * Delete all guest {@link SocialPerson} users.
  *
  * @author ceefour
  */
