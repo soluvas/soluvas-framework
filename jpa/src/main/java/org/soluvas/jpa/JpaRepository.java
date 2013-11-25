741265ed170d2375d586fa78bfdc4e1343e40841
Hendy Irawan <ceefour666@gmail.com>
2013-11-25 05:22:07 -0430

    HUB-9 exists methods

--- jpa/src/main/java/org/soluvas/jpa/JpaRepository.java
+++ jpa/src/main/java/org/soluvas/jpa/JpaRepository.java
@@ -1,8 +1,12 @@
 package org.soluvas.jpa;
 
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.Map;
 
 import org.soluvas.commons.Identifiable;
+import org.soluvas.data.Existence;
+import org.soluvas.data.StatusMask;
 import org.soluvas.data.repository.StatusAwareRepository;
 
 /**
@@ -11,5 +15,9 @@ import org.soluvas.data.repository.StatusAwareRepository;
  * @author ceefour
  */
 public interface JpaRepository<T extends JpaEntity<ID>, ID extends Serializable> extends StatusAwareRepository<T, ID> {
+
+	public Existence<String> existsById(StatusMask statusMask, String id);
+
+	public Map<String, Existence<String>> existsAllById(StatusMask statusMask, Collection<String> ids);
 
 }
