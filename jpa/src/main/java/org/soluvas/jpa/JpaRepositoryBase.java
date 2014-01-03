8d25a5e8dfd07c86b26456dc5341b127187304bd
Hendy Irawan <ceefour666@gmail.com>
2014-01-03 05:34:49 -0430

    FREQ-52

--- jpa/src/main/java/org/soluvas/jpa/JpaRepositoryBase.java
+++ jpa/src/main/java/org/soluvas/jpa/JpaRepositoryBase.java
@@ -143,8 +143,11 @@ public abstract class JpaRepositoryBase<T extends JpaEntity<ID>, ID extends Seri
 				return input.getBindableJavaType().getName();
 			}
 		}).toSet();
+//		Preconditions.checkArgument(entityNames.contains(entityClass.getName()),
+//				"EntityManager for '%s' JPA repository must include entity '%s'. Make sure you have set e.g. @PersistenceContext(unitName=\"fulan\"). %s known entities are: %s",
+//				entityClass.getSimpleName(), entityClass.getName(), entityNames.size(), entityNames);
 		Preconditions.checkArgument(entityNames.contains(entityClass.getName()),
-				"EntityManager for '%s' JPA repository must include entity '%s'. Make sure you have set e.g. @PersistenceContext(unitName=\"fulan\"). %s known entities are: %s",
+				"EntityManager for '%s' JPA repository must include entity '%s'. Make sure you have called factoryBean.setPackagesToScan(). %s known entities are: %s",
 				entityClass.getSimpleName(), entityClass.getName(), entityNames.size(), entityNames);
 		
 		this.log = LoggerFactory.getLogger(JpaRepositoryBase.class.getName() + "/" + entityClass.getSimpleName());
