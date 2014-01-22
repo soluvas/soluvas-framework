3082af9e0dd76f85cd7a7fd4c3381bdcb892cf57
Hendy Irawan <ceefour666@gmail.com>
2014-01-21 20:08:10 -0430

    OPEN - issue GRD-75: Email alerts/reminder notifications https://quikdo.atlassian.net/browse/GRD-75

--- schedule/src/main/java/org/soluvas/schedule/shell/SchedTrigLsCommand.java
+++ schedule/src/main/java/org/soluvas/schedule/shell/SchedTrigLsCommand.java
@@ -43,7 +43,7 @@ public class SchedTrigLsCommand extends ExtCommandSupport {
 		final DateTimeZone timeZone = appManifest.getDefaultTimeZone();
 		final Scheduler scheduler = getBean(Scheduler.class);
 		final Set<TriggerKey> triggerKeys = scheduler.getTriggerKeys(GroupMatcher.anyTriggerGroup());
-		System.out.println(ansi().render("@|negative_on %3s|%-35s|%-15s|%-8s|%-16s|%-16s|%-16s|%-16s|@",
+		System.out.println(ansi().render("@|negative_on %3s|%-40s|%-15s|%-8s|%-16s|%-16s|%-16s|%-16s|@",
 				"№", "Name", "Group", "State", "Start", "End", "Prev Fire", "Next Fire"));
 		final DateTimeFormatter formatter = new DateTimeFormatterBuilder().appendYear(4, 4).appendLiteral('-').appendMonthOfYear(2).appendLiteral('-').appendDayOfMonth(2)
 			.appendLiteral(' ').appendHourOfDay(2).appendLiteral(':').appendMinuteOfHour(2)
@@ -51,30 +51,41 @@ public class SchedTrigLsCommand extends ExtCommandSupport {
 		int i = 0;
 		for (final TriggerKey it : triggerKeys) {
 			final Trigger trigger = scheduler.getTrigger(it);
+			final String misfire; 
+			switch (trigger.getMisfireInstruction()) {
+			case Trigger.MISFIRE_INSTRUCTION_SMART_POLICY:
+				misfire = "mis=smart";
+				break;
+			case Trigger.MISFIRE_INSTRUCTION_IGNORE_MISFIRE_POLICY:
+				misfire = "mis=ignore";
+				break;
+			default:
+				misfire = "mis=" + trigger.getMisfireInstruction();
+			}
 			final TriggerState state = scheduler.getTriggerState(it);
 			final String triggerExpr;
 			if (trigger instanceof CronTrigger) {
-				triggerExpr = "cron " + ((CronTrigger) trigger).getCronExpression();
+				triggerExpr = "cron " + ((CronTrigger) trigger).getCronExpression() + " " + misfire;
 			} else if (trigger instanceof CalendarIntervalTrigger) {
 				final CalendarIntervalTrigger ciTrigger = (CalendarIntervalTrigger) trigger;
-				triggerExpr = "calendar " + ciTrigger.getRepeatInterval() + ciTrigger.getRepeatIntervalUnit();
+				triggerExpr = "calendar " + ciTrigger.getRepeatInterval() + ciTrigger.getRepeatIntervalUnit() + " " + misfire;
 			} else if (trigger instanceof DailyTimeIntervalTrigger) {
 				final DailyTimeIntervalTrigger dtiTrigger = (DailyTimeIntervalTrigger) trigger;
-				triggerExpr = "daily " + dtiTrigger.getRepeatInterval() + dtiTrigger.getRepeatIntervalUnit() + " " + dtiTrigger.getDaysOfWeek();
+				triggerExpr = "daily " + dtiTrigger.getRepeatInterval() + dtiTrigger.getRepeatIntervalUnit() + " " + dtiTrigger.getDaysOfWeek() + " " + misfire;
 			} else if (trigger instanceof SimpleTrigger) {
-				triggerExpr = "simple " + ((SimpleTrigger) trigger).getRepeatInterval() + "ms";
+				triggerExpr = "simple " + ((SimpleTrigger) trigger).getRepeatInterval() + "ms" + " " + misfire;
 			} else {
-				triggerExpr = trigger.toString();
+				triggerExpr = trigger.toString() + " " + misfire;
 			}
 			final JobDataMap jobDataMap = trigger.getJobDataMap();
-			System.out.println(ansi().render("@|bold,black %3d||@%-35s@|bold,black ||@%-15s@|bold,black ||@%-8s@|bold,black ||@%-16s@|bold,black ||@%-16s@|bold,black ||@%-16s@|bold,black ||@%-16s",
+			System.out.println(ansi().render("@|bold,black %3d||@%-40s@|bold,black ||@%-15s@|bold,black ||@%-8s@|bold,black ||@%-16s@|bold,black ||@%-16s@|bold,black ||@%-16s@|bold,black ||@%-16s",
 				++i, it.getName(), it.getGroup(), state,
 				trigger.getStartTime() != null ? formatter.print(new DateTime(trigger.getStartTime())) : null,
 				trigger.getEndTime() != null ? formatter.print(new DateTime(trigger.getEndTime())) : null,
 				trigger.getPreviousFireTime() != null ? formatter.print(new DateTime(trigger.getPreviousFireTime())) : null,
 				trigger.getNextFireTime() != null ? formatter.print(new DateTime(trigger.getNextFireTime())) : null
 			));
-			System.out.println(ansi().render("@|bold,black    ||@%-35s@|bold,black ||@%-24s@|bold,black ||@%s", triggerExpr, trigger.getJobKey().getName(), ImmutableMap.copyOf(jobDataMap)));
+			System.out.println(ansi().render("@|bold,black    ||@%-40s@|bold,black ||@%-24s@|bold,black ||@%s", triggerExpr, trigger.getJobKey().getName(), ImmutableMap.copyOf(jobDataMap)));
 		}
 		System.out.println(ansi().render("@|bold,yellow %d|@ Triggers (times are in @|bold %s|@)", 
 				triggerKeys.size(), timeZone));
