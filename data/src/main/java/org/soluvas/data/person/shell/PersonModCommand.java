fafc7552b5d7475e495071191245e4e1fada3441
Hendy Irawan <ceefour666@gmail.com>
2014-03-30 09:23:27 -0430

    HUB-38 #resolve

--- data/src/main/java/org/soluvas/data/person/shell/PersonModCommand.java
+++ data/src/main/java/org/soluvas/data/person/shell/PersonModCommand.java
@@ -30,7 +30,7 @@ import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
 
 /**
- * Shell command to edit {@link Person} in a {@link PersonRepository}.
+ * Shell command to edit {@link Person}(s) in a {@link PersonRepository}.
  *
  * @author ceefour
  */
@@ -50,78 +50,79 @@ public class PersonModCommand extends ExtCommandSupport {
 	private transient String slug;
 	@Option(name="-p", aliases="--password", description="Password (will be encoded as SSHA).")
 	private String password;
-
-	@Argument(index=0, name="id", required=true, description="Person ID.")
-	private transient String id;
+	@Argument(index=0, name="id ...", required=true, multiValued=true, description="Person ID(s).")
+	private transient String[] ids;
 	
 	@Override
-	protected Person doExecute() throws Exception {
+	protected String[] doExecute() throws Exception {
 		final PersonRepository personRepo = getBean(PersonRepository.class);
-		final Person person = Preconditions.checkNotNull(personRepo.findOne(id), String.format("Person by ID %s must not be null", id));
-		if (!Strings.isNullOrEmpty(emailStr)) {
-			final Person personByEmail = personRepo.findOneByEmail(StatusMask.RAW, emailStr);
-			if (personByEmail != null && !personByEmail.getId().equalsIgnoreCase(id)) {
-				 log.warn("Person by email {} is already exists by {}", emailStr, personByEmail.getId());
-			} else {
-				// remove last primary email AND the new email before adding a new one
-				String lastPrimaryEmail = person.getEmail();
-				final Iterator<Email> emailIter = person.getEmails().iterator();
-				while (emailIter.hasNext()) {
-					final Email email = emailIter.next();
-					if (lastPrimaryEmail != null && lastPrimaryEmail.equalsIgnoreCase(email.getEmail())) {
-						emailIter.remove();
-					} else if (emailStr.equalsIgnoreCase(email.getEmail())) {
-						// avoid duplicate by pre-removing existing new email address
-						// note that Iterator.remove() can only be called once
-						emailIter.remove();
+		for (final String id : ids) {
+			final Person person = Preconditions.checkNotNull(personRepo.findOne(id), String.format("Person by ID %s must not be null", id));
+			if (!Strings.isNullOrEmpty(emailStr)) {
+				final Person personByEmail = personRepo.findOneByEmail(StatusMask.RAW, emailStr);
+				if (personByEmail != null && !personByEmail.getId().equalsIgnoreCase(id)) {
+					 log.warn("Person by email {} is already exists by {}", emailStr, personByEmail.getId());
+				} else {
+					// remove last primary email AND the new email before adding a new one
+					String lastPrimaryEmail = person.getEmail();
+					final Iterator<Email> emailIter = person.getEmails().iterator();
+					while (emailIter.hasNext()) {
+						final Email email = emailIter.next();
+						if (lastPrimaryEmail != null && lastPrimaryEmail.equalsIgnoreCase(email.getEmail())) {
+							emailIter.remove();
+						} else if (emailStr.equalsIgnoreCase(email.getEmail())) {
+							// avoid duplicate by pre-removing existing new email address
+							// note that Iterator.remove() can only be called once
+							emailIter.remove();
+						}
 					}
+					
+					final Email email = CommonsFactory.eINSTANCE.createEmail();
+					email.setEmail(emailStr);
+					email.setPrimary(true);
+					email.setValidationTime(new DateTime());
+					person.getEmails().add(email);
 				}
-				
-				final Email email = CommonsFactory.eINSTANCE.createEmail();
-				email.setEmail(emailStr);
-				email.setPrimary(true);
-				email.setValidationTime(new DateTime());
-				person.getEmails().add(email);
 			}
-		}
-		
-		if (nameStr != null) {
-			final PersonName personName = NameUtils.splitName(nameStr);
-			person.setName(nameStr);
-			person.setFirstName(personName.getFirstName());
-			person.setLastName(personName.getLastName());
-		}
-		
-		if (gender != null) {
-			if (StringUtils.startsWithIgnoreCase(gender, "m")) {
-				gender = Gender.MALE.name();
-			} else if (StringUtils.startsWithIgnoreCase(gender, "f")) {
-				gender = Gender.FEMALE.name();
+			
+			if (nameStr != null) {
+				final PersonName personName = NameUtils.splitName(nameStr);
+				person.setName(nameStr);
+				person.setFirstName(personName.getFirstName());
+				person.setLastName(personName.getLastName());
 			}
-			person.setGender(Gender.valueOf(gender.toUpperCase()));
-		}
-		
-		if (!Strings.isNullOrEmpty(slug)) {
-			final Existence<String> existsBySlug = personRepo.existsBySlug(StatusMask.RAW, slug);
-			if (!existsBySlug.isPresent()) {
-				person.setSlug(slug);
-				person.setCanonicalSlug(SlugUtils.canonicalize(slug));
-			} else {
-				System.err.println(String.format("Slug '%s' already used by person '%s'", 
-						existsBySlug.get(), existsBySlug.getId()));
-				return null;
+			
+			if (gender != null) {
+				if (StringUtils.startsWithIgnoreCase(gender, "m")) {
+					gender = Gender.MALE.name();
+				} else if (StringUtils.startsWithIgnoreCase(gender, "f")) {
+					gender = Gender.FEMALE.name();
+				}
+				person.setGender(Gender.valueOf(gender.toUpperCase()));
 			}
+			
+			if (!Strings.isNullOrEmpty(slug)) {
+				final Existence<String> existsBySlug = personRepo.existsBySlug(StatusMask.RAW, slug);
+				if (!existsBySlug.isPresent()) {
+					person.setSlug(slug);
+					person.setCanonicalSlug(SlugUtils.canonicalize(slug));
+				} else {
+					System.err.println(String.format("Slug '%s' already used by person '%s'", 
+							existsBySlug.get(), existsBySlug.getId()));
+					return null;
+				}
+			}
+			
+			if (!Strings.isNullOrEmpty(password)) {
+				final String encoded = HashedPasswordUtils.encodeSsha(password);
+				System.err.println(ansi().render("Encoded password: @|bold %s|@", encoded));
+				person.setPassword(encoded);
+			}
+	
+			personRepo.modify(id, person);
 		}
 		
-		if (!Strings.isNullOrEmpty(password)) {
-			final String encoded = HashedPasswordUtils.encodeSsha(password);
-			System.err.println(ansi().render("Encoded password: @|bold %s|@", encoded));
-			person.setPassword(encoded);
-		}
-
-		personRepo.modify(id, person);
-		
-		return person;
+		return ids;
 	}
 
 }
