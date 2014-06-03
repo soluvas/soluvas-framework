4118fb478439836ca1d5c59cd36ef0d1a4ce40b0
Rudi Wijaya <rudi_mail90@yahoo.com>
2014-06-03 00:09:12 -0430

    BC-1915 #resolve. add for conditon STRING

--- jpa/src/main/java/org/soluvas/jpa/PersistentEnum.java
+++ jpa/src/main/java/org/soluvas/jpa/PersistentEnum.java
@@ -14,11 +14,18 @@ import org.postgresql.util.PGobject;
  * Map <a href="http://www.postgresql.org/">Postgres</a> <a href="http://wiki.postgresql.org/wiki/Enum">Enum</a>
  * onto <a href="http://download.oracle.com/javase/tutorial/java/javaOO/enum.html">Java Enum</a> using <a href="http://www.hibernate.org/">Hibernate</a>.
  * 
+ * @deprecated It seems only works with Hibernate 4.3 with PostgreSQL9Dialect, but a better version is already
+ * <a href="https://github.com/JadiraOrg/jadira/issues/14">available in Jadira 3.2.0</a>.
+ * For Hibernate 4.2 with PostgreSQL82Dialect, use Jadira {@link org.jadira.usertype.corejava.PersistentEnum}
+ * instead since PostgreSQL82Dialect returns {@code enum} as String, not as {@link PGobject}.
  * @author Rudi Wijaya <rudi@bippo.co.id>
  * @see http://anismiles.wordpress.com/2010/08/04/postgres-enum-with-hibernate/
  */
+@Deprecated
 public class PersistentEnum extends org.jadira.usertype.corejava.PersistentEnum {
 	
+	private static final long serialVersionUID = 1L;
+
 	@Override
 	public Object doNullSafeGet(ResultSet rs, String[] names,
 			SessionImplementor session, Object owner)
@@ -35,6 +42,8 @@ public class PersistentEnum extends org.jadira.usertype.corejava.PersistentEnum 
         if (identifier instanceof PGobject) {
             PGobject pg = (PGobject) identifier;
             return Enum.valueOf((Class) getMappedClass(), pg.getValue());
+        } else if (identifier instanceof String) { // quirk of PostgreSQL82Dialect? to write we must use PGobject, but to read it returns String??
+        	return Enum.valueOf((Class) getMappedClass(), (String) identifier);
         } else {
         	throw new IllegalArgumentException("PersistentEnum type expects PGobject, got " + identifier.getClass().getName() + " for value '" + identifier + "'");
         }
