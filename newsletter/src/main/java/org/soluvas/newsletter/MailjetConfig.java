b9dcab0ce1b89b22a47a6cf8a0d42c03e4226e20
Atang Sutisna <atang@bippo.co.id>
2014-06-23 02:03:02 -0430

    BC-1984 Fix Config in Spring 4

--- newsletter/src/main/java/org/soluvas/newsletter/MailjetConfig.java
+++ newsletter/src/main/java/org/soluvas/newsletter/MailjetConfig.java
@@ -1,25 +1,16 @@
 package org.soluvas.newsletter;
 
-import javax.annotation.Nullable;
 import javax.inject.Inject;
-import javax.inject.Named;
 
 import org.soluvas.commons.AppManifest;
-import org.soluvas.commons.config.CommonsWebConfig;
-import org.soluvas.commons.config.MultiTenantConfig;
 import org.soluvas.commons.config.SysConfigMapHolder;
-import org.soluvas.commons.config.TenantSelector;
 import org.soluvas.commons.tenant.TenantBeans;
-import org.soluvas.commons.tenant.TenantRepository;
 import org.soluvas.commons.tenant.TenantUtils;
 import org.soluvas.newsletter.impl.MailjetManagerImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Scope;
-
-import com.google.common.eventbus.EventBus;
 
 /**
  * {@link Configuration} for {@link Mailjet}.
@@ -30,15 +21,7 @@ import com.google.common.eventbus.EventBus;
 public class MailjetConfig {
 	
 	@Inject
-	private TenantSelector tenantSelector;
-	@Inject
-	private MultiTenantConfig tenantConfig;
-	@Inject @Named(CommonsWebConfig.APP_EVENT_BUS)
-	private EventBus appEventBus;
-	@Inject
 	private SysConfigMapHolder<? extends MailjetSysConfig> sysConfigMapHolder;
-	@Autowired(required=false) @Nullable
-	private TenantRepository<?> tenantRepo;
 	
 	@Bean(destroyMethod="destroy")
 	public TenantBeans<MailjetManager> mailjetMgrBeans() {
@@ -55,7 +38,7 @@ public class MailjetConfig {
 	
 	@Bean @Scope("prototype")
 	public MailjetManager mailjetMgr() {
-		return mailjetMgrBeans().get(tenantSelector.tenantRef().getTenantId());
+		return mailjetMgrBeans().getCurrent();
 	}
 	
 }
