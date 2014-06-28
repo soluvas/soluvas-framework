8021bc4170b2826e5c89ed1bed56d515c014ecab
Hendy Irawan <ceefour666@gmail.com>
2014-06-28 16:43:45 +0700

    TenantTransactionTemplate improvement

--- jpa/src/main/java/org/soluvas/jpa/TenantTransactionTemplate.java
+++ jpa/src/main/java/org/soluvas/jpa/TenantTransactionTemplate.java
@@ -3,6 +3,8 @@ package org.soluvas.jpa;
 import java.io.Closeable;
 import java.io.IOException;
 
+import javax.persistence.PersistenceException;
+
 import org.soluvas.commons.tenant.CommandRequestAttributes;
 import org.springframework.transaction.CannotCreateTransactionException;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -54,6 +56,9 @@ public class TenantTransactionTemplate extends TransactionTemplate {
 			throw new CannotCreateTransactionException(
 					String.format("%s» Cannot wrap transaction in session for tenant '%s': %s", 
 					tenantId, tenantId, e), e); 
+		} catch (PersistenceException e) {
+			// helper exception to show tenantId
+			throw new PersistenceException(tenantId + "» " + e.getMessage(), e);
 		}
 	}
 
