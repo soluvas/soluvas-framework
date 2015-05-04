a15f3e2818c75627472323d25c4336e87ee06eed
Rudi Wijaya <rudi_mail90@yahoo.com>
2015-05-04 00:03:49 -0430

    BC-3025 #comment test for findOne

---
+++ mongo/src/test/java/org/soluvas/mongo/MongoCustomerRoleRepositoryTest.java
@@ -0,0 +1,31 @@
+package org.soluvas.mongo;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.soluvas.commons.CustomerRole;
+import org.soluvas.data.customerrole.CustomerRoleRepository;
+
+/**
+ * @author rudi
+ *
+ */
+public class MongoCustomerRoleRepositoryTest {
+	
+	private static final Logger log = LoggerFactory
+			.getLogger(MongoCustomerRoleRepositoryTest.class);
+	
+	private final CustomerRoleRepository repo;
+
+	public MongoCustomerRoleRepositoryTest() {
+		repo = new MongoCustomerRoleRepository("mongodb://127.0.0.1:27017/tuneeca_dev", false, false);
+	}
+
+	@Test
+	public void findOneById() {
+		final String queryId = "common";
+		final CustomerRole customerRole = repo.findOne(queryId);
+		log.info("Got customerRole by id '{}': {}", queryId, customerRole);
+	}
+
+}
