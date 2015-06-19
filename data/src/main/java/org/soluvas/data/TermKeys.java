eeb633df002bb53135684fe849e70692233f6ebc
Rudi Wijaya <rudi_mail90@yahoo.com>
2015-06-18 23:41:23 -0430

    use enum-string

---
+++ data/src/main/java/org/soluvas/data/TermKeys.java
@@ -0,0 +1,14 @@
+package org.soluvas.data;
+
+/**
+ * @author rudi
+ *
+ */
+public class TermKeys {
+	
+	public static final String BASE_COLOR = "base_color";
+	
+	public static final String BASE_SIZE = "base_size";
+	
+
+}
