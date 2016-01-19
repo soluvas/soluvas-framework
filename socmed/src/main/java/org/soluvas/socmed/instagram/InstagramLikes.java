66da90e612f48fbe1bdd4103d8eef866952cb65d
Anton Kurniawan <anton.kurniawan.4892@gmail.com>
2016-01-19 06:11:53 -0430

    add like count

---
+++ socmed/src/main/java/org/soluvas/socmed/instagram/InstagramLikes.java
@@ -0,0 +1,20 @@
+package org.soluvas.socmed.instagram;
+
+public class InstagramLikes {
+
+	private Integer count;
+
+	public Integer getCount() {
+		return count;
+	}
+
+	public void setCount(Integer count) {
+		this.count = count;
+	}
+
+	@Override
+	public String toString() {
+		return "InstagramLikes [count=" + count + "]";
+	}
+	
+}
