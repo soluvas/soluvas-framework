4137d7f1d934ef0342438aa859b8b9999c5c56d2
Hendy Irawan <ceefour666@gmail.com>
2016-03-01 23:49:30 +0700

    LoggedInPersonInfoModel new

--- data/src/main/java/org/soluvas/data/person/JpaPersonRepository2.java
+++ data/src/main/java/org/soluvas/data/person/JpaPersonRepository2.java
@@ -1,7 +1,16 @@
 package org.soluvas.data.person;
 
+import org.soluvas.commons.Person;
+
+import java.util.Optional;
+
 /**
  * Created by ceefour on 01/03/2016.
  */
 public class JpaPersonRepository2 implements PersonRepository2 {
+    @Override
+    public Optional<Person> findOne(String tenantId, String personId) {
+        // FIXME: implement this
+        return Optional.empty();
+    }
 }
--- data/src/main/java/org/soluvas/data/person/PersonRepository2.java
+++ data/src/main/java/org/soluvas/data/person/PersonRepository2.java
@@ -1,9 +1,15 @@
 package org.soluvas.data.person;
 
+import org.soluvas.commons.Person;
+
+import java.util.Optional;
+
 /**
  * App-scoped and tenant-scoped aware successor of {@link PersonRepository}.
  * Created by ceefour on 01/03/2016.
  */
 public interface PersonRepository2 {
+
+    Optional<Person> findOne(String tenantId, String personId);
 
 }
