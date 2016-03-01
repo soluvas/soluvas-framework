7ed0f5b1b881b6d0fff5f9c03fa24f5f0001f829
Hendy Irawan <ceefour666@gmail.com>
2016-03-01 20:12:06 +0700

    JpaPersonRepository2

---
+++ data/src/main/java/org/soluvas/data/person/JpaPersonRepository2.java
@@ -0,0 +1,7 @@
+package org.soluvas.data.person;
+
+/**
+ * Created by ceefour on 01/03/2016.
+ */
+public class JpaPersonRepository2 implements PersonRepository2 {
+}
---
+++ data/src/main/java/org/soluvas/data/person/PersonRepository2.java
@@ -0,0 +1,9 @@
+package org.soluvas.data.person;
+
+/**
+ * App-scoped and tenant-scoped aware successor of {@link PersonRepository}.
+ * Created by ceefour on 01/03/2016.
+ */
+public interface PersonRepository2 {
+
+}
