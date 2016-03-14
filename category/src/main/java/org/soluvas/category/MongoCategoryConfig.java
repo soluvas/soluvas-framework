a2e047a16105819db4955218d6e48d97aa18cf0b
Rudi Wijaya <rudi_mail90@yahoo.com>
2016-03-14 04:36:49 -0430

    BC-3742 #comment use cacheable

--- category/src/main/java/org/soluvas/category/MongoCategoryConfig.java
+++ category/src/main/java/org/soluvas/category/MongoCategoryConfig.java
@@ -6,6 +6,7 @@ import org.soluvas.commons.AppManifest;
 import org.soluvas.commons.MongoSysConfig;
 import org.soluvas.commons.config.SysConfigMapHolder;
 import org.soluvas.commons.tenant.TenantBeans;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -22,6 +23,8 @@ public class MongoCategoryConfig {
 	private Environment env;
 	@Inject
 	private SysConfigMapHolder<? extends MongoSysConfig> sysConfigMapHolder;
+	@Inject
+	private CacheManager cacheMgr;
 	
 	@Bean
 	public TenantBeans<MongoCategoryRepository> mongoCategoryRepoBeans() {
@@ -32,7 +35,8 @@ public class MongoCategoryConfig {
 			protected MongoCategoryRepository create(String tenantId, AppManifest appManifest) throws Exception {
 				final MongoSysConfig sysConfig = sysConfigMapHolder.sysConfigMap().get(tenantId);
 				final String mongoUri = sysConfig.getMongoUri();
-				return new MongoCategoryRepositoryImpl(mongoUri, mongoMigrationEnabled, mongoAutoExplainSlow);
+				return new MongoCategoryRepositoryImpl(cacheMgr, tenantId,
+						mongoUri, mongoMigrationEnabled, mongoAutoExplainSlow);
 			}
 		};
 	}
