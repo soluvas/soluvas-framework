1149ea3706c919b88cb1f14e18192870c52473bd
Atang Sutisna <atang@bippo.co.id>
2016-10-11 18:39:06 +0700

    BC-4080: bug fixing instagram -caption

--- socmed/src/main/java/org/soluvas/socmed/instagram/InstagramCaption.java
+++ socmed/src/main/java/org/soluvas/socmed/instagram/InstagramCaption.java
@@ -3,8 +3,10 @@ package org.soluvas.socmed.instagram;
 public class InstagramCaption {
 
 	private String text;
-	private InstagramCaption caption;
+//	private InstagramCaption caption;
 	private String created_time;
+	private InstagramUser from;
+	private String id;
 
 	public String getText() {
 		return text;
@@ -14,13 +16,13 @@ public class InstagramCaption {
 		this.text = text;
 	}
 
-	public InstagramCaption getCaption() {
-		return caption;
-	}
-
-	public void setCaption(InstagramCaption caption) {
-		this.caption = caption;
-	}
+//	public InstagramCaption getCaption() {
+//		return caption;
+//	}
+//
+//	public void setCaption(InstagramCaption caption) {
+//		this.caption = caption;
+//	}
 
 	public String getCreated_time() {
 		return created_time;
@@ -29,10 +31,21 @@ public class InstagramCaption {
 	public void setCreated_time(String created_time) {
 		this.created_time = created_time;
 	}
-
-	@Override
-	public String toString() {
-		return "InstagramCaption [text=" + text + ", caption=" + caption + ", created_time=" + created_time + "]";
+	
+	public void setFrom(InstagramUser from) {
+		this.from = from;
+	}
+	
+	public InstagramUser getFrom() {
+		return from;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getId() {
+		return id;
 	}
 	
 }
