5f8e6cdb1da30ba79cbc9d34708539d93d7c3152
Rudi Wijaya <rudi_mail90@yahoo.com>
2017-01-25 02:58:18 -0400

    reverted..

--- commons/src/main/java/org/soluvas/commons/tenant/TenantXmiBeansImpl.java
+++ commons/src/main/java/org/soluvas/commons/tenant/TenantXmiBeansImpl.java
@@ -130,18 +130,22 @@ public class TenantXmiBeansImpl<T extends EObject> implements TenantRepositoryLi
 	protected File getFileFor(String tenantId) {
 		final ImmutableMap<String, Object> scope = ImmutableMap.<String, Object>of(
 				"tenantId", tenantId, "tenantEnv", tenantConfig.getTenantEnv(), 
-				"dataDir", tenantConfig.dataDirMap().get(tenantId).toURI().toString().replace("file:/", ""));
+				"dataDir", tenantConfig.dataDirMap().get(tenantId).toString());
+//				"dataDir", tenantConfig.dataDirMap().get(tenantId).toURI().toString().replace("file:/", "")); ini tuk windows malah duplicate "home dir"-nya
 		final String path;
 		try {
-			path = pathTemplate.expand(scope).replace("file://", "");
+			path = pathTemplate.expand(scope);
+//			path = pathTemplate.expand(scope).replace("file://", "");ini tuk windows malah duplicate "home dir"-nya
 		} catch (Exception e) {
 			throw new TenantException(e, "Cannot get %s file path for tenant '%s' template: %s scope: %s",
 					getClass().getName(), tenantId, pathTemplate.getTemplate(), scope);
 		}
 		try {
-			return new File(path);
+			return new File(URI.create(path));
+//			return new File(path);ini tuk windows malah duplicate "home dir"-nya
 		} catch (Exception e) {
-			throw new TenantException(e, "Invalid %s file path '%s' for tenant '%s' template: %s scope: %s",
+			throw new TenantException(e, "Invalid %s file URI '%s' for tenant '%s' template: %s scope: %s",
+//			throw new TenantException(e, "Invalid %s file path '%s' for tenant '%s' template: %s scope: %s",ini tuk windows malah duplicate "home dir"-nya
 					getClass().getName(), path, tenantId, pathTemplate.getTemplate(), scope);
 		}
 	}
