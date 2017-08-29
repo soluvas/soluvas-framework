fe6cf9ad4b0ddd02262061a8329acb11bea14d32
Atang Sutisna <atang@bippo.co.id>
2017-08-29 16:54:16 +0700

    check before put to mailjet

--- newsletter/src/main/java/org/soluvas/newsletter/handler/PersonSubscribedToMailJetHandler.java
+++ newsletter/src/main/java/org/soluvas/newsletter/handler/PersonSubscribedToMailJetHandler.java
@@ -2,11 +2,12 @@ package org.soluvas.newsletter.handler;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.soluvas.commons.Person;
 import org.soluvas.commons.entity.Person2;
 import org.soluvas.mongo.event.PersonSubscribedToMailJetEvent;
 import org.soluvas.newsletter.Mailjet;
 import org.soluvas.newsletter.MailjetManager;
+
+import com.google.common.base.Preconditions;
 
 /**
  * @author atang
@@ -30,10 +31,14 @@ public class PersonSubscribedToMailJetHandler {
 
 	public void subcribeToMailJet(PersonSubscribedToMailJetEvent ev) {
 		final Person2 person = ev.getPerson();
+		log.debug("Attempting to put person id  {} with email ({}) to subscriber contact", person.getId(), person.getEmail());
 		try {
-			log.debug("Subcribing {}({}) to Mail Jet", person.getId(), person.getEmail());
+			Preconditions.checkState(person.getNewsletterSubscriptionEnabled(), 
+					String.format("Person with name/email {}/{} dont want to be subscriber", person.getName(), person.getEmail()));
 			final Mailjet mailjet = new Mailjet(mailJetMgr);
 			mailjet.addContact(mailJetMgr.getListId(), person.getEmail());
+		} catch (IllegalStateException e) {
+			log.info("Adding contact {}/{} list is canceled", person.getName(), person.getEmail());
 		} catch (Exception e) {
 			log.error("Can not subribe {} with email {}", person.getId(), person.getEmail());
 		}
