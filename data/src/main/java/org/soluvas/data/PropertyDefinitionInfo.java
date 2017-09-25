97593419a74995c2045975325fdd1b5ce3b6cd53
eki <eki@bippo.co.id>
2017-09-25 18:29:45 +0700

    cpp

---
+++ data/src/main/java/org/soluvas/data/PropertyDefinitionInfo.java
@@ -0,0 +1,74 @@
+package org.soluvas.data;
+
+import java.io.Serializable;
+
+import javax.measure.unit.Unit;
+
+/**
+ * @author eki
+ *
+ */
+public class PropertyDefinitionInfo implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	public final static String NAME_ATTR = "name";
+	public final static String DESCRIPTION_ATTR = "description";
+	
+	private String id;
+	private String name;
+	private String defaultEnum;
+	private PropertyKind defaultKind;
+	private Unit<?> defaultUnit;
+	
+	public PropertyDefinitionInfo() {
+		super();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDefaultEnum() {
+		return defaultEnum;
+	}
+
+	public void setDefaultEnum(String defaultEnum) {
+		this.defaultEnum = defaultEnum;
+	}
+
+	public PropertyKind getDefaultKind() {
+		return defaultKind;
+	}
+
+	public void setDefaultKind(PropertyKind defaultKind) {
+		this.defaultKind = defaultKind;
+	}
+
+	public Unit<?> getDefaultUnit() {
+		return defaultUnit;
+	}
+
+	public void setDefaultUnit(Unit<?> defaultUnit) {
+		this.defaultUnit = defaultUnit;
+	}
+
+	@Override
+	public String toString() {
+		return "PropertyDefinitionInfo [id=" + id + ", name=" + name + ", defaultEnum=" + defaultEnum + ", defaultKind="
+				+ defaultKind + ", defaultUnit=" + defaultUnit + "]";
+	}
+	
+}
