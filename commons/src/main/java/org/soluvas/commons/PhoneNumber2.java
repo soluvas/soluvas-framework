cbecc5a74f3adcfe82fc2120494e75ffb015cf65
Atang Sutisna <atang@bippo.co.id>
2019-01-23 13:27:42 +0700

    add id for phone numbber

--- commons/src/main/java/org/soluvas/commons/PhoneNumber2.java
+++ commons/src/main/java/org/soluvas/commons/PhoneNumber2.java
@@ -3,16 +3,21 @@
 package org.soluvas.commons;
 
 import java.io.Serializable;
+import java.util.UUID;
 
 import javax.persistence.Embeddable;
 
 import org.joda.time.DateTime;
+
+import com.google.code.morphia.annotations.Id;
 
 /**
  * Type of <a href="https://schema.org/telephone">schema:telephone</a>.
  */
 @Embeddable
 public class PhoneNumber2 implements Serializable {
+	@Id
+	protected String id;
     protected String phoneNumber;
     protected Boolean primary;
     protected DateTime validationTime;
@@ -23,8 +28,17 @@ public class PhoneNumber2 implements Serializable {
 
 	public PhoneNumber2(String phoneNumber, Boolean primary) {
 		super();
+		this.id = UUID.randomUUID().toString();
 		this.phoneNumber = phoneNumber;
 		this.primary = primary;
+	}
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
 	}
 
 	public String getPhoneNumber() {
