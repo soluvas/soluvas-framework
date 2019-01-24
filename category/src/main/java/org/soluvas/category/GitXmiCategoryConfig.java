c32ba3b6b903a3d0ea8abb8c9d678481247dec20
Atang Sutisna <atang@bippo.co.id>
2019-01-24 12:13:45 +0700

    xmicategorydeprecated

--- category/src/main/java/org/soluvas/category/GitXmiCategoryConfig.java
+++ category/src/main/java/org/soluvas/category/GitXmiCategoryConfig.java
@@ -31,30 +31,30 @@ public class GitXmiCategoryConfig {
 	@Inject
 	private Environment env;
 	
-	@Bean
-	public TenantBeans<CategoryRepository> categoryRepoBeans() {
-		return new TenantBeans<CategoryRepository>(GitXmiCategoryRepository.class) {
-			@Override
-			protected GitXmiCategoryRepository create(String tenantId, AppManifest appManifest)
-					throws Exception {
-				final File dataDir = tenantConfig.dataDirMap().get(tenantId);
-				final EventBus tenantEventBus = TenantUtils.selectBean(tenantId, tenantConfig.eventBusMap(), EventBus.class);
-				final TenantDirProvision tenantDirProvision = env.getRequiredProperty("tenantDirProvision", TenantDirProvision.class);
-				final File tenantXmiFile = new File(dataDir, "common/" + tenantId + ".CategoryCatalog.xmi");
-				final Map<String, File> xmiFiles = tenantXmiFile.exists() ? ImmutableMap.of(tenantId, tenantXmiFile) : ImmutableMap.<String, File>of();
-//					final XmiCategoryRepository repo = new XmiCategoryRepository( 
-//							ImmutableList.<URL>of(), xmiFiles, eventBus);
-				final GitXmiCategoryRepository repo = new GitXmiCategoryRepository( 
-						ImmutableList.<URL>of(), xmiFiles, tenantEventBus, tenantDirProvision);
-				return repo;
-				
-			}
-		};
-	}
-	
-	@Bean @Scope("prototype")
-	public CategoryRepository categoryRepo() {
-		return categoryRepoBeans().getCurrent();
-	}
+//	@Bean
+//	public TenantBeans<CategoryRepository> categoryRepoBeans() {
+//		return new TenantBeans<CategoryRepository>(GitXmiCategoryRepository.class) {
+//			@Override
+//			protected GitXmiCategoryRepository create(String tenantId, AppManifest appManifest)
+//					throws Exception {
+//				final File dataDir = tenantConfig.dataDirMap().get(tenantId);
+//				final EventBus tenantEventBus = TenantUtils.selectBean(tenantId, tenantConfig.eventBusMap(), EventBus.class);
+//				final TenantDirProvision tenantDirProvision = env.getRequiredProperty("tenantDirProvision", TenantDirProvision.class);
+//				final File tenantXmiFile = new File(dataDir, "common/" + tenantId + ".CategoryCatalog.xmi");
+//				final Map<String, File> xmiFiles = tenantXmiFile.exists() ? ImmutableMap.of(tenantId, tenantXmiFile) : ImmutableMap.<String, File>of();
+////					final XmiCategoryRepository repo = new XmiCategoryRepository( 
+////							ImmutableList.<URL>of(), xmiFiles, eventBus);
+//				final GitXmiCategoryRepository repo = new GitXmiCategoryRepository( 
+//						ImmutableList.<URL>of(), xmiFiles, tenantEventBus, tenantDirProvision);
+//				return repo;
+//				
+//			}
+//		};
+//	}
+//	
+//	@Bean @Scope("prototype")
+//	public CategoryRepository categoryRepo() {
+//		return categoryRepoBeans().getCurrent();
+//	}
 	
 }
