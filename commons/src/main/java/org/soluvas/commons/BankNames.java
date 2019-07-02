2450fc076fc6d7ff785db3f97830278e18306107
Atang Sutisna <atang@bippo.co.id>
2019-07-02 05:26:54 -0400

    BC-5018: add banks

--- commons/src/main/java/org/soluvas/commons/BankNames.java
+++ commons/src/main/java/org/soluvas/commons/BankNames.java
@@ -32,8 +32,31 @@ public class BankNames {
 	
 	public static final String BANK_PERMATA = "BANK_PERMATA";
 	
+    public static final String BCA_SYARIAH = "BCA SYARIAH";
+
+    public static final String BNI_SYARIAH = "BNI SYARIAH";
+
+    public static final String BRI_SYARIAH = "BRI SYARIAH";
+
+    public static final String MEGA = "MEGA";
+
+    public static final String BJB = "BJB";
+
+    public static final String BTPN = "BTPN";
+
+    public static final String BANK_BUKOPIN = "BANK BUKOPIN";
+
+    public static final String BANK_CIMB_NIAGA = "BANK CIMB NIAGA";
+
+    public static final String BANK_OCBC_NISP = "BANK OCBC NISP";
+
+    public static final String BANK_MUAMALAT = "BANK MUAMALAT";
+
+    public static final String BANK_PANIN = "BANK PANIN";
+	
 	public static final List<String> ALL = ImmutableList.copyOf(new String[] { 
-			BCA, BNI, BRI, BTN, DANAMON, BANK_JABAR, BANK_MANDIRI, BANK_PERMATA});
+			BCA, BNI, BRI, BTN, DANAMON, BANK_JABAR, BANK_MANDIRI, BANK_PERMATA, BCA_SYARIAH, BNI_SYARIAH, BRI_SYARIAH, MEGA, BJB, BTPN,
+			BANK_BUKOPIN, BANK_CIMB_NIAGA, BANK_OCBC_NISP, BANK_MUAMALAT, BANK_PANIN});
 	
 	public static final Map<String, String> DISPLAY_NAMES;
 	
@@ -47,5 +70,18 @@ public class BankNames {
 		DISPLAY_NAMES.put(BANK_JABAR, "BANK JABAR");
 		DISPLAY_NAMES.put(BANK_MANDIRI, "BANK MANDIRI");
 		DISPLAY_NAMES.put(BANK_PERMATA,"BANK PERMATA");
+		
+		DISPLAY_NAMES.put(BCA_SYARIAH,"BCA SYARIAH");
+		DISPLAY_NAMES.put(BNI_SYARIAH,"BNI SYARIAH ");
+		DISPLAY_NAMES.put(BRI_SYARIAH,"BRI SYARIAH");
+		DISPLAY_NAMES.put(MEGA,"MEGA");
+		DISPLAY_NAMES.put(BJB,"BJB");
+		DISPLAY_NAMES.put(BTPN,"BTPN");
+		DISPLAY_NAMES.put(BANK_BUKOPIN,"BANK BUKOPIN");
+		DISPLAY_NAMES.put(BANK_CIMB_NIAGA,"BANK CIMB NIAGA");
+		DISPLAY_NAMES.put(BANK_OCBC_NISP,"BANK OCBC NISP");
+		DISPLAY_NAMES.put(BANK_MUAMALAT,"BANK MUAMALAT");
+		DISPLAY_NAMES.put(BANK_PANIN,"BANK PANIN");
 	}
+	
 }
